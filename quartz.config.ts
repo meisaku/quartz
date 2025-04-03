41e50c6d0ddbf0c5f18083552c1dcdd88dc00b0d
Saku Mikkola <saku@aptual.fi>
2025-04-03 15:56:50 +0300

    Changed: Font fix

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,7 +24,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         header: "Schibsted Grotesk",
-        body: "Corisande",
+        body: "Oxygen",
         code: "IBM Plex Mono",
       },
       colors: {
