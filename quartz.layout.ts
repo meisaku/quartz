1fbe8ca22ea3275d328f75086e2352bfc23b3881
Saku Mikkola <saku@aptual.fi>
2025-04-04 08:34:49 +0300

    Changed: Updated links

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,8 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      "Ilmoita havainnoistasi": "https://must-have.johku.com/",
+      "Johku.fi": "https://johku.fi",
     },
   }),
 }
